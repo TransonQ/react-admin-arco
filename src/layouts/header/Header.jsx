8cebd7e084c7d64ccf6766a6ef05e6c2522395fd
quanscheng <quanscheng@163.com>
2022-10-30 22:48:02 +0800

    feat: 侧边栏

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,3 +1,59 @@
+import Logo from '@/assets/logo.svg'
+import styles from './Header.module.less'
+
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.left}>
+        <div className={styles.logo}>
+          <Logo />
+          <div className={styles['logo-name']}>Arco Pro</div>
+        </div>
+      </div>
+      <ul className={styles.right}>
+        {/* <li>
+          <Select
+            triggerElement={<IconButton icon={<IconLanguage />} />}
+            options={[
+              { label: '中文', value: 'zh-CN' },
+              { label: 'English', value: 'en-US' },
+            ]}
+            value={lang}
+            triggerProps={{
+              autoAlignPopupWidth: false,
+              autoAlignPopupMinWidth: true,
+              position: 'br',
+            }}
+            trigger="hover"
+          />
+        </li>
+
+        <li>
+          <Tooltip
+            content={
+              theme === 'light'
+                ? t['settings.navbar.theme.toDark']
+                : t['settings.navbar.theme.toLight']
+            }
+          >
+            <IconButton
+              icon={theme !== 'dark' ? <IconMoonFill /> : <IconSunFill />}
+              onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            />
+          </Tooltip>
+        </li>
+        <li>
+          <Dropdown droplist={droplist} position="br" disabled={userLoading}>
+            <Avatar size={32} style={{ cursor: 'pointer' }}>
+              {userLoading ? (
+                <IconLoading />
+              ) : (
+                <img alt="avatar" src={userInfo.avatar} />
+              )}
+            </Avatar>
+          </Dropdown>
+        </li> */}
+      </ul>
+    </div>
+  )
 }
--- src/layouts/main/Main.jsx
+++ src/layouts/main/Main.jsx
@@ -1,14 +1,38 @@
-import { Button, Layout } from '@arco-design/web-react'
+import { Layout, Menu } from '@arco-design/web-react'
+import { IconMenuFold, IconMenuUnfold } from '@arco-design/web-react/icon'
+import { useCallback, useState } from 'react'
 import { Header } from '../header/Header'
 import styles from '../style/layout.module.less'
 
 export const Main = () => {
+  const [collapsed, setCollapsed] = useState(false)
+  const toggleCollapse = useCallback(
+    () => setCollapsed((collapsed) => !collapsed),
+    []
+  )
+
   return (
     <Layout className={styles.layout}>
       <div className={styles['layout-navbar']}>
         <Header />
       </div>
-      <Button>123</Button>
+      <Layout>
+        <Layout.Sider
+          className={styles['layout-sider']}
+          collapsed={collapsed}
+          onCollapse={setCollapsed}
+          trigger={null}
+          collapsible
+          breakpoint="xl"
+        >
+          <div className={styles['menu-wrapper']}>
+            <Menu collapse={collapsed}></Menu>
+          </div>
+          <div className={styles['collapse-btn']} onClick={toggleCollapse}>
+            {collapsed ? <IconMenuUnfold /> : <IconMenuFold />}
+          </div>
+        </Layout.Sider>
+      </Layout>
     </Layout>
   )
 }
