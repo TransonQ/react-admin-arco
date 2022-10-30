587adc92974ba99572686c2266787376d2f4632e
quanscheng <quanscheng@163.com>
2022-10-30 22:18:49 +0800

    ci: 使用customize-cra进入覆盖cra配置

---
+++ config-overrides.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const {
+  override,
+  addWebpackModuleRule,
+  addWebpackPlugin,
+  addWebpackAlias,
+} = require('customize-cra')
+const ArcoWebpackPlugin = require('@arco-plugins/webpack-react')
+const addLessLoader = require('customize-cra-less-loader')
+
+module.exports = {
+  webpack: override(
+    addWebpackAlias({
+      '@': path.resolve(__dirname, 'src'),
+    }),
+    addLessLoader({
+      lessLoaderOptions: {
+        lessOptions: {},
+      },
+    }),
+    addWebpackModuleRule({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+    }),
+    addWebpackPlugin(
+      new ArcoWebpackPlugin({
+        theme: '@arco-themes/react-quange',
+      })
+    )
+  ),
+}
